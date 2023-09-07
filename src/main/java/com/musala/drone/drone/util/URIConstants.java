755e8300e6a476b1eef6423761c79e28eff61b44
Enrique Basto <enrique.muguercia2016@gmail.com>
2023-09-07 09:34:55 -0400

    adding new constants

--- src/main/java/com/musala/drone/drone/util/URIConstants.java
+++ src/main/java/com/musala/drone/drone/util/URIConstants.java
@@ -3,4 +3,6 @@ package com.musala.drone.drone.util;
 public class URIConstants {
     public static final String API_BASE_URL="/api/v1";
     public static final String AUTHENTICATION_RESOURCE_PATH="/security";
+    public static final String DRONE_RESOURCE_PATH="/drone";
+    public static final String MEDICATION_RESOURCE_PATH="/medication";
 }
