2f0399870c90536b3439c00325a291346e8fd4f6
Enrique Basto <enrique.muguercia2016@gmail.com>
2023-09-06 23:41:07 -0400

    creating application database models

--- src/main/java/com/musala/drone/drone/DroneApplication.java
+++ src/main/java/com/musala/drone/drone/DroneApplication.java
@@ -2,8 +2,10 @@ package com.musala.drone.drone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DroneApplication {
 
 	public static void main(String[] args) {
