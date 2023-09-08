c2f7d3ee657145e375b5c1c3e88b7bd71993ff3b
Enrique Basto <enrique.muguercia2016@gmail.com>
2023-09-08 02:06:38 -0400

    adding dummy data to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (100,'LIGHTWEIGHT', '000000001', 'IDLE',100 ,1);
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (90, 'MIDDLEWEIGHT', '000000001', 'IDLE',200 ,2);
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (80, 'MIDDLEWEIGHT', '000000001', 'IDLE',300 ,3);
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (70, 'CRUSIERWEIGHT', '000000001', 'IDLE',400 ,4);
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (60, 'HEAVYWEIGHT', '000000001', 'IDLE',100 ,5);
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (50, 'HEAVYWEIGHT', '000000001', 'IDLE',200,6 );
+insert into drone (battery, model, serial_number, state, weight,drone_id) values (20, 'HEAVYWEIGHT', '000000001', 'IDLE',500,7 );
+
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_999','file', 'dipirona', 140,1);
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_9900','file', 'multivitaminas', 150,2);
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_9990','file', 'inyeccion',160,3);
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_99999','file', 'advil', 170,4);
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_9999','file', 'lolo', 180,5);
+insert into medication (code, image, name, weight,medication_id) values ('FFFF_999', 'file', 'test', 190,6);
