171085fec9a3ff30d683e3b07f413a600976e694
Enrique Basto <enrique.muguercia2016@gmail.com>
2023-09-08 02:19:12 -0400

    enabling scheduled

--- src/main/java/com/musala/drone/drone/DroneApplication.java
+++ src/main/java/com/musala/drone/drone/DroneApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(StorageProperties.class)
+@EnableScheduling
 public class DroneApplication {
 
 	public static void main(String[] args) {
